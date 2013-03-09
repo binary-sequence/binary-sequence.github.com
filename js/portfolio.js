41a0752203cb4502c41c3a424320dfb310be9188
sergio@sergiomini <sergiolindo.empresa@gmail.com>
2013-03-10 00:12:19 +0100

    Basic portfolio.

---
+++ js/portfolio.js
@@ -0,0 +1,59 @@
+;
+$(document).ready(function() {
+
+	$('#aApps').click(function(e) {
+
+		e.preventDefault();
+
+		$('#aApps').removeClass('selected');
+		$('#aVideogames').removeClass('selected');
+		$('#aCV').removeClass('selected');
+
+		$('#sectionVideogames').hide(0);
+		$('#sectionCV').hide(0, function() {
+
+			$('#sectionApps').show(0);
+			$('#aApps').addClass('selected');
+
+		});
+
+	});
+
+	$('#aVideogames').click(function(e) {
+
+		e.preventDefault();
+
+		$('#aApps').removeClass('selected');
+		$('#aVideogames').removeClass('selected');
+		$('#aCV').removeClass('selected');
+
+		$('#sectionApps').hide(0);
+		$('#sectionCV').hide(0, function() {
+
+			$('#sectionVideogames').show(0);
+			$('#aVideogames').addClass('selected');
+
+		});
+
+	});
+
+	$('#aCV').click(function(e) {
+
+		e.preventDefault();
+
+		$('#aApps').removeClass('selected');
+		$('#aVideogames').removeClass('selected');
+		$('#aCV').removeClass('selected');
+
+		$('#sectionApps').hide(0);
+		$('#sectionVideogames').hide(0, function() {
+
+			$('#sectionCV').show(0);
+			$('#aCV').addClass('selected');
+
+		});
+
+	});
+
+	$('#aVideogames').click();
+});
